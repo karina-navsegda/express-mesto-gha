ee9bbe9bc2a79d5cd87068e106d7d9796301c8c1
kkkarina <apresmoi@inbox.ru>
2023-08-25 14:47:08 +0300

    3

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,14 @@
-const User = require("../models/user");
+const User = require('../models/user');
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.status(201).send(user))
     .catch((err) => {
-      if (err.name === "ValidationError") {
+      if (err.name === 'ValidationError') {
         res.status(400).send({ message: err.message });
       } else {
-        res.status(500).send({ message: "Произошла ошибка" });
+        res.status(500).send({ message: 'Произошла ошибка' });
       }
     });
 };
@@ -16,7 +16,7 @@ module.exports.createUser = (req, res) => {
 module.exports.getUser = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
-    .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.getUserId = (req, res) => {
@@ -24,46 +24,46 @@ module.exports.getUserId = (req, res) => {
     User.findById(req.params.userId)
       .then((user) => {
         if (!user) {
-          res.status(404).send({ message: "Пользователь не найден" });
+          res.status(404).send({ message: 'Пользователь не найден' });
           return;
         }
         res.send(user);
       })
-      .catch(() => res.status(404).send({ message: "Пользователь не найден" }));
+      .catch(() => res.status(404).send({ message: 'Пользователь не найден' }));
   } else {
-    res.status(400).send({ message: "Пользователь не найден" });
+    res.status(400).send({ message: 'Пользователь не найден' });
   }
 };
 
 module.exports.editUser = (req, res) => {
   const { name, about } = req.body;
   if (req.user._id) {
-    User.findByIdAndUpdate(req.user._id, { name, about })
+    User.findByIdAndUpdate(req.user._id, { name, about }, { runValidators: true, new: 'true' })
       .then((user) => res.send(user))
       .catch((err) => {
-        if (err.name === "ValidationError") {
+        if (err.name === 'ValidationError') {
           res.status(400).send({ message: err.message });
         } else {
-          res.status(404).send({ message: "Пользователь не существует" });
+          res.status(404).send({ message: 'Пользователь не существует' });
         }
       });
   } else {
-    res.status(500).send({ message: "Произошла ошибка" });
+    res.status(500).send({ message: 'Произошла ошибка' });
   }
 };
 
 module.exports.editAvatar = (req, res) => {
   if (req.user._id) {
-    User.findByIdAndUpdate(req.user._id, { avatar: req.body.avatar })
+    User.findByIdAndUpdate(req.user._id, { avatar: req.body.avatar }, { runValidators: true, new: 'true' })
       .then((user) => res.send(user))
       .catch((err) => {
-        if (err.name === "ValidationError") {
+        if (err.name === 'ValidationError') {
           res.status(400).send({ message: err.message });
         } else {
-          res.status(404).send({ message: "Пользователь не существует" });
+          res.status(404).send({ message: 'Пользователь не существует' });
         }
       });
   } else {
-    res.status(500).send({ message: "Произошла ошибка" });
+    res.status(500).send({ message: 'Произошла ошибка' });
   }
 };
