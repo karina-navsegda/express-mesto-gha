d2e464fc3516b307f48f1127f3cff76fac343435
kkkarina <apresmoi@inbox.ru>
2023-09-11 02:43:06 +0300

    5

--- routes/users.js
+++ routes/users.js
@@ -14,21 +14,25 @@ const {
 } = require('../controllers/users');
 
 router.get('/', getUser);
+
 router.get('/:userId', celebrate({
   params: Joi.object().keys({
     userId: Joi.string().length(24).required(),
   }),
 }), getUserId);
+
 router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
   }),
 }), editUser);
+
 router.get('/me', getUserMe);
+
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
-    avater: Joi.string().pattern(linkRegex),
+    avatar: Joi.string().pattern(linkRegex),
   }),
 }), editAvatar);
 
